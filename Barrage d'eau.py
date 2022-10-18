ec2543714608bc5785935b2638c15e628959c2db
HAKIMA ARKHA <115365535+hakima2022@users.noreply.github.com>
2022-10-18 21:42:39 +0200

    Add files via upload

---
+++ Barrage d'eau.py
@@ -0,0 +1,7 @@
+h=float(input("entrer la valeur de la hauteur en métre:"))
+MV=float(input("donner la valeur de la masse volumique:"))
+S=float(input("entrer la valeur de la surface:"))
+p=h*MV*9.80
+print(p)
+F=p*S
+print(F)
